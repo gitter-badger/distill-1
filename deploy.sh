05aece28f8c99d46d4de4c21e16b93f57a69d8b5
Tim Trice <tim.trice@gmail.com>
2019-06-30 06:53:10 -0500

    Build docs in develop branch

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git config --global user.name "Travis CI"
 git config --global user.email "tim.trice@gmail.com"
 git clone https://github.com/timtrice/distill.git
 cd distill
-git checkout master
+git checkout develop
 Rscript -e 'rmarkdown::render_site(".");'
 
 if [ ! -d "docs" ]
@@ -14,9 +14,7 @@ then
   exit 1
 fi
 
-cd docs
-git init
 git add .
 MSG="Rebuild website, $(date)"
 git commit -m "$MSG"
-git push --force $FULL_REPO master:gh-pages
+git push --force $FULL_REPO develop
