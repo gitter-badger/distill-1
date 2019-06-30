c3a65ca8da997290442b4116204bcd9fb482f1a8
Tim Trice <tim.trice@gmail.com>
2019-06-29 19:39:59 -0500

    Add functions to handle Remotes

--- R/01_install_packages.R
+++ R/01_install_packages.R
@@ -1,2 +1,19 @@
 install.packages("remotes")
 remotes::install_deps(dependencies = "Depends")
+
+# Get Remotes
+remotes <- remotes::local_package_deps(dependencies = "Remotes")
+
+if (!is.null(remotes)) {
+  purrr::walk(
+    .x = strsplit(
+      x = remotes::local_package_deps(dependencies = "Remotes"),
+      split = "::"
+    ),
+    .f = ~rlang::exec(
+      .fn = utils::getFromNamespace(x = paste0("install_", .x[1]), ns = "remotes"),
+      repo = .x[2],
+      upgrade = "never"
+    )
+  )
+}
